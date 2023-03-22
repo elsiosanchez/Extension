41445365d380204040c75722b088086262e2506f
elsiosanchez <45974454+elsiosanchez@users.noreply.github.com>
2023-03-22 17:17:16 -0400

    1 file modified, 2 added

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
 <template>
-  <molecula-Paginations :background="true" />
+  <el-button type="primary" plain>
+    <NuxtLink to="/" class="n-link-base"> Home </NuxtLink>
+  </el-button>
+  <el-button type="primary" plain>
+    <NuxtLink to="/about"> About </NuxtLink>
+  </el-button>
+  <el-card shadow="never">
+    <p style="text-align: center">
+      Current route: <code>{{ route.path }}</code>
+    </p>
+  </el-card>
 </template>
+<script setup lang="ts">
+const route = useRoute();
+</script>
